e8370921341892ae98e5458d76837407dce667a6
kamiyechung <46262732+kamiyechung@users.noreply.github.com>
2019-01-14 21:40:18 +0100

    Simon says ok

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -18,6 +18,9 @@ def first_word(a)
     a.split.first
 end
 
-def titleize (word)
-    word.capitalize 
+def titleize(string_to_capitalize)
+ string_to_capitalize.split.collect!.with_index {
+  |cur_word, index|
+  (cur_word.length > 3 || index == 0) ? cur_word.capitalize : cur_word
+ }.to_a.join(' ')
 end
