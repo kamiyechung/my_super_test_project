75e693edfa03ccd113bb237902431c5d53099876
Jordan Aumis <jordan0aumis@gmail.com>
2019-01-14 11:37:28 +0100

    temperature sans le putain de within de merde

---
+++ lib/01_temperature.rb
@@ -0,0 +1,16 @@
+
+degres = gets.chomp.to_i
+def ftoc(degres)
+ puts fareinheit = (degres -32) * 5/9
+ return fareinheit
+end
+ftoc(degres)
+
+degresc = gets.chomp.to_f.round(1)
+
+def ctof(degresc)
+    puts celsius = (degresc * 9/5) + 32
+    return celsius
+   end
+   ctof(degresc)
+
